3fc83d1082042415678292b21f77a57f9a27f77e
Cash <cashwugeek@icloud.com>
2024-08-14 12:11:11 +0800

    fix post

--- app/Main.tsx
+++ app/Main.tsx
@@ -21,7 +21,6 @@ export default function Home({ posts }) {
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            console.log(post)
             const { slug, date, title, summary, tags, draft } = post
             return (
               <li key={slug} className="py-12">
