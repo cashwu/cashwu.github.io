ad7c0ad3482bdbce53b8053654e903490dc8cf2b
cash <cashwugeek@icloud.com>
2025-07-30 16:19:39 +0800

    fix google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6615686078290194"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
